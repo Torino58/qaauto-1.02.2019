a12ce92ef9a2508b214a7edb478f87aff775119e
Ilya Zilinskiy <zilinsiy@gmail.com>
2019-02-19 21:22:33 +0200

    Lesson 6

---
+++ src/test/java/LoginSubmitPage.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginSubmitPage {
+
+    private WebDriver driver;
+
+    private WebElement loginForm;
+    private WebElement passwordValidationMessage;
+    private WebElement emailValidationMessage;
+
+
+
+
+    public LoginSubmitPage(WebDriver driver) {
+        this.driver = driver;
+        initElements();
+
+    }
+
+    private void initElements(){
+
+        loginForm = driver.findElement(By.xpath("//form[@class='login__form']"));
+        passwordValidationMessage = driver.findElement(By.xpath("//div[@id='error-for-password']"));
+        emailValidationMessage = driver.findElement(By.xpath("//div[@id='error-for-username']"));
+
+
+    }
+
+
+    public  boolean isPageLoaded(){
+
+        return loginForm.isDisplayed()
+                && driver.getCurrentUrl().contains("/login-submit")
+                && driver.getTitle().equals("Войти в LinkedIn");
+
+
+    }
+
+    public String getPasswordValidationMessageText() {
+        return passwordValidationMessage.getText();
+    }
+
+    public String getEmailValidationMessage() {
+        return emailValidationMessage.getText();
+    }
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,16 +1,50 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\SkillUP_Student\\Desktop\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.close();
+    }
+
+    @DataProvider
+    public Object[][] validData() {
+        return new Object[][]{
+                { "irvold66@ukr.net", "pbkbycrbq555" },
+                { "irvold66@ukr.NET", "pbkbycrbq555" },
+                { " irvold66@ukr.net ", "pbkbycrbq555" }
+
+        };
+    }
+
+    @Test(dataProvider = "validData")
+    public void successfulLoginTest() {
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
+        landingPage.login("irvold66@ukr.net", "pbkbycrbq555");
+
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isPageLoaded(), "Home page did not load after Login");
+
+
+    }
 
     @Test
     public void negativeEmptyPasswordTest() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\SkillUP_Student\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
 
@@ -22,16 +56,11 @@ public class LoginTests {
 
     @Test
     public void negativeIncorrectPasswordTest() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\SkillUP_Student\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
         landingPage.login("irvold66@ukr.net", "123");
 
-        IncorrectLoginPasswordPage incorrectLoginPasswordPage = new IncorrectLoginPasswordPage(driver);
+        LoginSubmitPage incorrectLoginPasswordPage = new LoginSubmitPage(driver);
         Assert.assertTrue(incorrectLoginPasswordPage.isPageLoaded(), "Incorrect Login?Password Page is not loaded");
 
 
@@ -39,56 +68,55 @@ public class LoginTests {
 
     @Test
     public void negativeIncorrectLoginTest() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\SkillUP_Student\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
         landingPage.login("irvold66@ukr.nes", "pbkbycrbq555");
 
-        IncorrectLoginPasswordPage incorrectLoginPasswordPage = new IncorrectLoginPasswordPage(driver);
-        Assert.assertTrue(incorrectLoginPasswordPage.isPageLoaded(), "Incorrect Login?Password Page is not loaded");
+        LoginSubmitPage loginSubmitPage = new LoginSubmitPage(driver);
+        Assert.assertTrue(loginSubmitPage.isPageLoaded(), "Incorrect Login?Password Page is not loaded");
 
 
     }
 
-
-
     @Test
     public void negativeInvalidLoginTest() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\SkillUP_Student\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
         landingPage.login("irvold66#ukr.net", "pbkbycrbq555");
 
-        IncorrectLoginPasswordPage incorrectLoginPasswordPage = new IncorrectLoginPasswordPage(driver);
+        LoginSubmitPage incorrectLoginPasswordPage = new LoginSubmitPage(driver);
         Assert.assertTrue(incorrectLoginPasswordPage.isPageLoaded(), "Incorrect Login?Password Page is not loaded");
 
 
     }
 
-
     @Test
-    public void successfulLoginTest() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\SkillUP_Student\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
-
+    public void negativePasswordReturnedToLoginSubmitTest() {
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
-        landingPage.login("irvold66@ukr.net", "pbkbycrbq555");
+        landingPage.login("irvoldd66@ukr.net", "pb2bycrbq555");
 
-        HomePage homePage = new HomePage(driver);
-        Assert.assertTrue(homePage.isPageLoaded(), "Home page did not load after Login");
+        LoginSubmitPage loginSubmitPage = new LoginSubmitPage(driver);
+        Assert.assertTrue(loginSubmitPage.isPageLoaded(), "Home page did not load after Login");
+        Assert.assertEquals(loginSubmitPage.getPasswordValidationMessageText(),
+                "Это неверный пароль. Повторите попытку или измените пароль.",
+                "Wrong validation message for password field");
 
 
     }
 
+    @Test
+    public void negativeLoginReturnedToLoginSubmitTest() {
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded");
+        landingPage.login("irvoldd66@ukr.net", "pbkbycrbq555");
+
+        LoginSubmitPage loginSubmitPage = new LoginSubmitPage(driver);
+        Assert.assertTrue(loginSubmitPage.isPageLoaded(), "Home page did not load after Login");
+        Assert.assertEquals(loginSubmitPage.getEmailValidationMessage(),
+                "Этот адрес эл. почты не зарегистрирован в LinkedIn. Повторите попытку.",
+                "Wrong validation message for password field");
+
+
+    }
 }
