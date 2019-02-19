3a92de1f50e7387b32303903d0f3306057a78367
Ilya Zilinskiy <zilinsiy@gmail.com>
2019-02-19 15:21:55 +0200

    Lesson 5.4

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -47,7 +47,7 @@ public class LoginTests {
         landingPage.login("irvold662ukr.net", "pbkbycrbq555");
 
         IncorrectLoginPasswordPage incorrectLoginPasswordPage = new IncorrectLoginPasswordPage(driver);
-        Assert.assertTrue(incorrectLoginPasswordPage.isPageLoaded(), "Incorrect Login?Password Page is not loadedssss");
+        Assert.assertTrue(incorrectLoginPasswordPage.isPageLoaded(), "Incorrect Login?Password Page is not loadedsss111111111s");
 
 
     }
